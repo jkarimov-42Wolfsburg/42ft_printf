69718fa626fc90d6030de82b90d62d21e071449c
Jasur Karimov <jkarimov@c2s18d110.42wolfsburg.de>
2024-01-24 15:48:47 +0100

    main function is added

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jkarimov <jkarimov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 14:44:13 by jkarimov          #+#    #+#             */
-/*   Updated: 2024/01/08 19:45:52 by jkarimov         ###   ########.fr       */
+/*   Updated: 2024/01/24 15:47:31 by jkarimov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,3 +58,41 @@ int	ft_printf(const char *s, ...)
 	va_end(args);
 	return (count);
 }
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	char			c = 'h';
+// 	char			*s = "Hello, World!";
+// 	void			*p = (void *)0x7fff5694dc58;
+// 	int				nbr	= 42;
+// 	unsigned int	n = 2024;
+// 	unsigned int	hex_lower = 0xabcd;
+// 	unsigned int	hex_upper = 0xABCD;
+
+// //printing single char
+// 	printf("Standard: %c\n", c);
+// 	ft_printf("Mine: %c\n", c);
+// //printing string
+// 	printf("Standard: %s\n", s);
+// 	ft_printf("Mine: %s\n", s);
+// //printing pointer
+// 	printf("Standard: %p\n", (void *)p);
+// 	ft_printf("Mine: %p\n", (void *)p);
+// //printing digit or int
+// 	printf("Standard: %d\n Standard: %i\n", nbr, nbr);
+// 	ft_printf("Mine: %d\n Mine %i\n", nbr, nbr);
+// //printing unsigned int
+// 	printf("Standard: %u\n", n);
+// 	ft_printf("Mine: %u\n", n);
+// //printing hex value in lowercase
+// 	printf("Standard: %x\n", hex_lower);
+// 	ft_printf("Mine: %x\n", hex_lower);
+// //printing hex value in uppercase
+// 	printf("Standard: %X\n", hex_upper);
+// 	ft_printf("Mine: %X\n", hex_upper);
+// //printing % sign
+// 	printf("Standard: %%\n");
+// 	ft_printf("Mine: %%\n");
+// }
